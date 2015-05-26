39e79680c13821fe6062b967bc9ce3307ea48fc9
MattiaGianotti <dragonmat1992@live.it>
2015-05-26 09:34:46 +0200

    sistemato errore nel js

--- js/loadAllCourseCategory.js
+++ js/loadAllCourseCategory.js
@@ -21,11 +21,11 @@ function getFromDB(id){
             var categories=JSON.parse(response);
             var result = "<div class='row'>";
             var j = 0;
-            for(var i = 0; i < category.length; i++){
+            for(var i = 0; i < categories.length; i++){
                 if(j%3 == 0){
                     result = result + "</div> <div class='row'>";
                 }
-                result = result + " <div class='col-xs-12 col-sm-6 col-md-4'> <div class='thumbnail'> <img src='img/categories/category-thumbnail-"+ category[i] + ".jpg' alt='Image not available, sorry.' class='img-responsive'><div class='caption'> <h3>" + category[i].name + "</h3><a href='category.html?id=" + category[i].id + "'>See more details</a> <br> <a href='courseofcategory.html?category=" + category[i].name + "'>All courses of this category</a></div></div></div>";
+                result = result + " <div class='col-xs-12 col-sm-6 col-md-4'> <div class='thumbnail'> <img src='img/categories/category-thumbnail-"+ categories[i].name + ".jpg' alt='Image not available, sorry.' class='img-responsive'><div class='caption'> <h3>" + categories[i].name + "</h3><a href='category.html?id=" + categories[i].id + "'>See more details</a> <br> <a href='courseofcategory.html?category=" + categories[i].name + "'>All courses of this category</a></div></div></div>";
                 j++;
             }
             result = result + "</div></div>";
